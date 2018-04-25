e68592eb309d6aa84b3aed337a34019ab41d9dbd
Lapsho <evillapsho@gmail.com>
2018-04-25 20:44:37 +0300

    додав для користувача вивід бажаної кількості зображень (використав тернарний оператор, здійснив перевірку isset() для глобальної змінної, перевірив на пустоту константу перед її виводом)

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
-<?php
-  include "random_image.php";  									//підключив random_image.php
-?>
 <html>
+
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <?php echo title ?>     									     <!-- застосував константу title -->
+  <?php require_once "random_image.php";  ?>
+  <title>	<?php if(!empty(title)) {echo title;} else{echo "лажа";} ?>	</title>    <!-- перевірив та застосував константу title -->
   <link rel="stylesheet" href="css/style.css">
-
 </head>
+
+
 <body>
+
+  <form method="GET">
+  	<p>Введіть кількість зображень, що потрібно відобразити: <input type="number" name="max_num_img" />
+  	<input type="submit" value="завантажити"></p>
+  </form>
 
   <p class="imglist">
   
      <?php
 
-     	for($a = 0; $a < 9; $a++){												//використав цикл for для автоматизації виводу зображень
+     	for($a = 0; $a < $max_num_img; $a++){	//використав цикл for для автоматизації виводу зображень
 
-    	just_for_lulz($img_arr);												//Застосував функцію генерування рандомних посилань
- 		echo "<a href=\"" . $fetch_precious . "\" data-fancybox=\"images\">";  
-        echo "<img src=\"" . $fetch_precious . "\" />";     					//повернув значення з вище застосованої функції в межає синтаксису html
-        echo "</a>";
+    	just_for_lulz($img_arr);				//Застосував функцію генерування рандомних посилань
+ 		echo $fetch_precious;					//вся необхідна для виведення в html писанина міститься в одній змінній	
 
      	}
      ?>
@@ -32,4 +35,4 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
 
 </body>
- </html> 
+ </html>
