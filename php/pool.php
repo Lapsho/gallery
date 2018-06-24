3baaba117bd8b74d4d4bdcfc042c0b2d9b3d759a
ubuntu <ubuntu@ubuntu.dev>
2018-06-24 16:39:05 +0300

    remove garbage

--- php/pool.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "php/enterpriseLogic/constants.php";
-require_once "php/enterpriseLogic/errorsRequest.php";
-require_once "php/enterpriseLogic/gallery.php";
-require_once "php/enterpriseLogic/pagination.php";
-require_once "php/enterpriseLogic/rest.php";
-require_once "php/enterpriseLogic/thumbnail.php";
-require_once "php/enterpriseLogic/validationUpload.php";
